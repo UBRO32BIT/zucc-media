afc399c407a7f938546154b640fbf11e217d70b1
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2024-01-19 23:47:44 +0700

    implemented view posts list

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,19 +1,25 @@
 import { Post } from "./post/Post";
 import { db } from "../config/firebase";
 import { getDocs, collection } from "firebase/firestore";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const PostList = () => {
     const postsRef = collection(db, "posts");
-    const [postList, setPostList] = useState(null);
+    const [postList, setPostList] = useState<Post[] | null>(null);
     const getPosts = async () => {
         const data = await getDocs(postsRef);
-        console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        setPostList(
+            data.docs.map((doc) => ({...doc.data(), id: doc.id})) as Post[]
+        );
     }
-    getPosts();
+    useEffect(() => {
+        getPosts(); // Move getPosts into useEffect to avoid potential issues
+    }, []); // Empty dependency array to ensure it runs only once
     return (
         <>
-            <Post/>
+            {postList?.map((post) => (
+                <Post key={post.id} {...post}/>
+            ))}
         </>
     );
 }
--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,12 +1,21 @@
-export const Post = () => {
+export interface Post {
+    id: string,
+    userId: string,
+    title: string,
+    username: string,
+    description: string,
+}
+
+export const Post = (props: Post) => {
+    const { id, userId, title, username, description } = props;
     return (
         <div>
             <div>
-                <h5>username</h5>
+                <h5>{username}</h5>
             </div>
-            <h2>POST TITLE</h2>
+            <h2>{title}</h2>
             <div>
-                <p>Post content</p>
+                <p>{description}</p>
             </div>
         </div>
     );
