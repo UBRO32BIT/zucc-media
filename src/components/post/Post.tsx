3e922588b48bb29a47312071a32a5eff96f89134
UBRO32BIT <90371372+UBRO32BIT@users.noreply.github.com>
2024-01-20 23:36:35 +0700

    test card component

--- src/components/post/Post.tsx
+++ src/components/post/Post.tsx
@@ -1,3 +1,7 @@
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import { Typography } from '@mui/material';
+
 export interface Post {
     id: string,
     userId: string,
@@ -9,14 +13,19 @@ export interface Post {
 export const Post = (props: Post) => {
     const { id, userId, title, username, description } = props;
     return (
-        <div>
-            <div>
-                <h5>{username}</h5>
-            </div>
-            <h2>{title}</h2>
-            <div>
-                <p>{description}</p>
-            </div>
-        </div>
+        <Box sx={{ minWidth: 275 }}>
+            <Card variant="outlined">
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    {username}
+                </Typography>
+                <Typography variant="h5" component="div">
+                    {title}
+                </Typography>
+                <Typography variant="h6" color="text.secondary">
+                    {description}
+                </Typography>
+            </Card>
+            
+        </Box>
     );
 }
